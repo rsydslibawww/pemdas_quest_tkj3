c3c2984c336e2e1eaf70fc0cfc2010ab01bc83d9
RASYAD <112366944+rsydslibawww@users.noreply.github.com>
2022-09-08 13:20:25 +0700

    Add files via upload

---
+++ python-latihan.py
@@ -0,0 +1,38 @@
+# Progam Dinamis
+
+print("\n1. Luas Persegi")
+sisi = int(input("Masukan nilai sisi: "))
+luas_P = sisi * sisi
+print("Luas Pesegi adalah", luas_P)
+
+print("\n2. Segitiga")
+Alas = int(input("Masukan nilai Alas: "))
+Tinggi = int(input("Masukan nilai Tinggi: "))
+luas_S = Alas * Tinggi / 2
+print("Luas Segitiga adalah", luas_S)
+
+
+print("\n3. Jajar Genjang")
+Alas = int(input("Masukan nilai Alas: "))
+Tinggi = int(input("Masukan nilai Tinggi: "))
+luas_J = Alas * Tinggi
+print("Luas Jajar Genjang adalah", luas_J)
+
+
+print("\n4. Lingkaran")
+Phi = 22/7
+r1 = int(input("Masukan nilai r1: "))
+r2 = int(input("Masukan nilai r2: "))
+luas_L = Phi * r1 * r2
+print("Luas Lingkaran adalah", luas_L)
+
+
+print("\n5. persegi Panjang")
+Panjang = int(input("Masukan nilai Panjang: "))
+Lebar = int(input("Masukan nilai Lebar: "))
+luas_PP = Panjang * Lebar
+print("Luas Lingkaran adalah", luas_PP)
+
+
+
+
